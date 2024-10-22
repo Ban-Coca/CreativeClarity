41e136b57ffe418f2bdddc4265e3f1711f2f7cb3
BanCoca <harveycoca0219@gmail.com>
2024-10-22 15:50:43 +0800

    Appdev + React Project

---
+++ clarity_springboot/src/main/java/com/creative_clarity/clarity_springboot/ClaritySpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.creative_clarity.clarity_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaritySpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClaritySpringbootApplication.class, args);
+	}
+
+}
