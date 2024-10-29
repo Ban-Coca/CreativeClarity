7b8da15a1d8367c0a65aceebca4dbd669733c63f
kimasaph <kim.bacaltos@cit.edu>
2024-10-29 16:15:12 +0800

    Update App.jsx

--- creativeclarity_frontend/src/App.jsx
+++ creativeclarity_frontend/src/App.jsx
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more Brand=cg
+        Click on the Vite and React logos to learn more Brave New World
       </p>
     </>
   )
